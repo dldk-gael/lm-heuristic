39d8c56c8331f09898146e71b935a072d3a6f20b
dldk-gael <gael@de-leseleuc.com>
2020-04-30 11:19:10 +0200

    clean MCTS example

--- src/test/test_mcts.py
+++ src/test/test_mcts.py
@@ -7,11 +7,13 @@ import random
 
 random.seed(3)
 GRAMMAR_FOLDER = 'data/cfg/'
-BATCH_SIZE = 16
+GRAMMAR_NAME = 'ex_2'
+
+BATCH_SIZE = 16  #
 
 if __name__ == '__main__':
     # Load grammar
-    with open(GRAMMAR_FOLDER+'ex_1.cfg') as f:
+    with open(GRAMMAR_FOLDER + GRAMMAR_NAME + '.cfg') as f:
         str_grammar = f.read()
     grammar = nltk.CFG.fromstring(str_grammar)
 
@@ -19,16 +21,17 @@ if __name__ == '__main__':
     gpt_2_scorer = GPT2Score('gpt2', batch_size=BATCH_SIZE, length_normalization=True)
     heuristic = lambda terminal_nodes: gpt_2_scorer(list(map(str, terminal_nodes)))
 
-    #heuristic = lambda terminal_nodes: [0] * len(terminal_nodes)
-
-    # Prepare root node
+    # Prepare grammar tree
     root = Derivation(grammar.start(), grammar)
 
-    # Initialize and perform the search
+    # Initialize the search parameters
     mtcs = MonteCarloTreeSearch(root, evaluation_fn=heuristic, batch_size=BATCH_SIZE,
-                                nb_of_tree_walks=15, c=1, d=1000, t=0)
+                                nb_of_tree_walks=200, c=1, d=1000, t=0)
 
+    # Perform the search and print some info
     best_node = mtcs.search()
     mtcs.print_search_info()
 
+    # Example on how to print more info on internal node located on the best path
     mtcs.node_info(1)
+
