1b701771826d912474fef446ccbdcebc3e57b2e1
dldk-gael <gael@de-leseleuc.com>
2020-06-10 14:42:32 +0200

    remove prolog import

--- src/lm_heuristic/tree/__init__.py
+++ src/lm_heuristic/tree/__init__.py
@@ -3,4 +3,3 @@ from .cfg_node import CFGrammarNode
 from .counter_node import CounterNode
 from .tree_stats import TreeStats
 from .feature_grammar_node import FeatureGrammarNode
-from .pl_node import PrologGrammarNode
