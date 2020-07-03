7bbd31ed97a907f9005e93bab47af581cd6bbf7e
dldk-gael <gael@de-leseleuc.com>
2020-07-03 16:44:05 +0200

    doc time

--- src/lm_heuristic/utils/timer.py
+++ src/lm_heuristic/utils/timer.py
@@ -1,69 +1,71 @@
+"""
+Define various tools to keep track of times
+"""
+
 import time
 from functools import wraps
 
-
-def timeit(func):
-    """
-    In order to evaluate the time spent in a class :
-    - make the class inheritate from Timer
-    - decorate the method you want to track time with @timeit
-    :param func: method to decorate
-    """
-
-    def inner(self, *args, **kwargs):
-        begin_time = time.perf_counter()
-        result = func(self, *args, **kwargs)
-        self._time_spent += time.perf_counter() - begin_time
-        return result
-
-    return inner
+import torch
 
 
-class Timer:
-    """
-    Timer class allows to keep track of time spent in methods that are decorated with @timeit
-    """
+######################################################################
+## Define context manager that keep track of the time spent inside
+######################################################################
 
-    def __init__(self):
-        self._time_spent = 0
+class TimeGPUComputation:
+    def __init__(self, step_name):
+        self.step_name = step_name
+        assert torch.cuda.is_available(), "Try to track GPU computation but cuda is not available"
 
-    def reset_timer(self):
-        self._time_spent = 0
+    def __enter__(self):
+        self.start_event = torch.cuda.Event(enable_timing=True)
+        self.end_event = torch.cuda.Event(enable_timing=True)
+        self.start_event.record()
 
-    def time_spent(self):
-        return self._time_spent
+    def __exit__(self, *args, value, traceback):
+        self.end_event.record()
+        torch.cuda.synchronize()
+        elapsed_time_ms = self.start_event.elapsed_time(self.end_event)
+        print("%s : %f ms" % (self.step_name, elapsed_time_ms))
 
-
-## CONTEXT MANAGER THAT COMPUTE TIME SPENT INSIDE 
 
 class TimeComputation:
     """
-    Context manager that compute time spent inside 
+    Context manager that compute time spent inside
     """
+
     def __init__(self, step_name):
-        self.step_name = step_name 
+        self.step_name = step_name
 
     def __enter__(self):
         self.begin_time = time.process_time()
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, *args):
         end_time = time.process_time()
         elapsed_time_ms = (end_time - self.begin_time) * 1000
         print("%s : %f ms" % (self.step_name, elapsed_time_ms))
 
 
-## DECORATOR THAT KEEP TRACK OF TIME SPENT INSIDE
+######################################################################
+## Define decorators that keep track of the time spent inside 
+## a function
+######################################################################
 
 def time_function(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        begin_time = time.process_time() 
+        begin_time = time.process_time()
         res = func(*args, **kwargs)
         end_time = time.process_time()
         wrapper.time_spent += end_time - begin_time
         return res
+
     wrapper.time_spent = 0.0
     return wrapper
+
+######################################################################
+## Other time utility function
+######################################################################
 
 def print_timer(func):
     print("Time spent in %s : %0.2f ms" % (func.__name__, func.time_spent * 1000))
