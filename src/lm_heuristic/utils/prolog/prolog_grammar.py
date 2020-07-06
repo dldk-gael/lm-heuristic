726edf4f30b52f4d6c105a0a9a4cc155e58a0ab7
dldk-gael <gael@de-leseleuc.com>
2020-07-06 13:40:08 +0200

    change structure in prolog folder

--- src/lm_heuristic/utils/prolog/prolog_grammar.py
+++ src/lm_heuristic/utils/prolog/prolog_grammar.py
@@ -1,8 +1,52 @@
 from typing import List, Union, Set
-from pyswip import Prolog
-from lm_heuristic.prolog.utils import format_term, join
-from lm_heuristic.prolog.parser import parse_to_prolog
+from pyswip import Functor, Prolog
+from .parser import parse_to_prolog
 
+######################################################################
+## Some util functions
+######################################################################
+
+def format_value(value):
+    output = ""
+    if isinstance(value, list):
+        output = "[" + ", ".join([format_value(val) for val in value]) + "]"
+    elif isinstance(value, Functor) and value.arity == 2:
+        output = "{0}{1}{2}".format(value.args[0], value.name, value.args[1])
+    else:
+        output = "{}".format(value)
+
+    return output
+
+
+def format_term(term):
+    if isinstance(term, list):
+        return [format_term(x) for x in term]
+    else:
+        return term.value
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def join(symbols):
+    """
+    Use to join symbol with "," and add "'" arround number
+    """
+    if len(symbols) == 0:
+        return ""
+    virgule = ", " if len(symbols) > 1 else ""
+    symbol = "'%s'" % symbols[0] if is_number(symbols[0]) else symbols[0]
+    return symbol + virgule + join(symbols[1:])
+
+
+######################################################################
+## Interface towards a Prolog engine (swi-prolog) by using pyswip
+######################################################################
 
 class PrologGrammarEngine:
     """
